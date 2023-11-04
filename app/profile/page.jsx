d841485d4b80baa54a141f250243bfe367e11b8a
Karanvir Brar <brarkaran938@gmail.com>
2023-11-03 21:49:15 -0500

    Fixes to editing and deleting of prompts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -20,7 +20,7 @@ const MyProfile = () => {
     if(hasConfirmed)
     {
         try {
-            await fetch(`/api/promp/${post._id.toString()}`, {
+            await fetch(`/api/prompt/${post._id.toString()}`, {
                 method: 'DELETE',
             });
 
